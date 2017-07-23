6dd3f3df65c61a17ed166fa63d110f6f99923e63
Jimmy <jameskinyua590@gmail.com>
2017-07-23 12:42:50 +0300

    Define a view function to facilitate creation of a new topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """
@@ -25,3 +28,22 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
 
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """
+    Add a new topic.
+    """
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+        
+        
