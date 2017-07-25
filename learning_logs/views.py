04721d732f0a8a94df2fc1761dd937dbe59020e5
Jimmy <jameskinyua590@gmail.com>
2017-07-25 14:52:18 +0300

    Restrict a topic to be viewed only by the owner of the topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 
@@ -27,6 +27,10 @@ def topic(request, topic_id):
     Show a single topic and all its entries.
     """
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+        
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
 
